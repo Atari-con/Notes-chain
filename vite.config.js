cfe11a69415563cc8839c2f78d2998958bb8860b
Atari-con <zhongcha125@gmail.com>
2025-08-29 00:51:38 +0600

    fix: react router rewrites

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 5000,
+  },
+  build: {
+    outDir: "dist",
+  },
+  // 👇 вот это ключ
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
